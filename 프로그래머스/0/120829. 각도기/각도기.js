7374e46ff8dca289c9e5aa0dba458d639effd66a
wt0329 <62666929+wt0329@users.noreply.github.com>
2024-12-18 13:58:36 +0900

    Update 각도기.js

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -11,3 +11,16 @@ function solution(angle) {
     }
     return answer;
 }
+
+/*
+ - 다른 사람 풀이
+ function solution(angle) {
+    var answer = 0;
+    return [0, 90, 91, 180].filter(x => angle >= x).length;
+ }
+ 
+ * filter : 특정 요소를 만족하는 요소를 걸러주고 배열로 반환하는 함수
+ 
+ * 설명 : 배열에 있는 수를 차례대로 비교하면서 angle >= x가 만족하는지 보면 return 값이 나옴
+ 
+*/
