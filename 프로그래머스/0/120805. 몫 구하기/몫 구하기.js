3a8d41fdc53c3af1ac691ab347a5136f228e0bcf
wt0329 <62666929+wt0329@users.noreply.github.com>
2024-12-18 13:20:54 +0900

    [level 0] Title: 몫 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -0,0 +1,7 @@
+function solution(num1, num2) {
+    var answer = 0;
+    if (num1 >= 0 && num1 <= 100 && num2 >= 0 && num2 <= 100) {
+        answer = parseInt(num1 / num2);
+    }
+    return answer;
+}
