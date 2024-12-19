325b28ee9a921e834018ac42a064f3c25dce9965
wt0329 <62666929+wt0329@users.noreply.github.com>
2024-12-19 14:02:07 +0900

    Update 가격대 별 상품 개수 구하기.sql

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -3,4 +3,10 @@ SELECT TRUNCATE(PRICE, -4) AS "PRICE_GROUP"
      , COUNT(*) AS "PRODUCTS"
   FROM PRODUCT 
  GROUP BY PRICE_GROUP
- ORDER BY PRICE_GROUP
+ ORDER BY PRICE_GROUP ASC;
+
+ /*
+   - TRUNCATE 
+     TRUNCATE(수, [자릿수]) : 수를 지정된 소수점 자리까지 버림하여 반환하는 함수
+                            , 기본값은 0, 자릿수가 음수일 경우 지정된 정수부에서 버림
+ */
